a31368835986db676b883d7f3d6687b3e61e5f43
Joshua Weinstein <jweinstein@splunk.com>
2019-03-11 12:46:29 -0700

    added unit test tool

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,4 @@
 // Main testing file
+var unit_test = require("./unit_test.js");
 
 console.log("TESTS PASSED")
---
+++ tests/unit_test.js
@@ -0,0 +1,49 @@
+/**
+ * This file contains tools to help write unit test cases.
+ * These are meant to be run in the node runtime, not inside a browser.
+ */
+
+ var UnitTestCase = (function(){
+ 	function UnitTestCase(name, fn, expected){
+ 		this.tname = name;
+ 		this.fn = fn;
+ 		this.expected = expected;
+ 		this.result = null;
+ 		this.passed = false;
+ 	}
+
+ 	UnitTestCase.prototype.testSingleArg = function(arg) {
+ 		this.result = this.fn(arg);
+ 	};
+
+ 	UnitTestCase.prototype.testMultiArg = function() {
+ 		this.result = this.fn.apply(null, arguments);
+ 	};
+
+ 	UnitTestCase.prototype.validate = function() {
+ 		if(this.result === this.expected) {
+ 			this.passed = true;
+ 		} else {
+ 			// result is kept as false.
+ 			return false;
+ 		}
+ 	};
+
+ 	UnitTestCase.prototype.method_name = function() {
+ 		var repString = "Unit Test(" + this.tname + ") ";
+ 		return repString + "expected: " + this.expected +
+ 		                 " result: " + this.result.toString() +
+ 		                 (this.passed ? " PASSED" : " FAILED");
+ 	};
+
+ 	UnitTestCase.prototype.reset = function() {
+ 		this.result = null;
+ 		this.passed = false;
+ 	};
+
+
+ 	return UnitTestCase;
+ })();
+
+// export of unit test case.
+ exports.UnitTestCase = UnitTestCase;
